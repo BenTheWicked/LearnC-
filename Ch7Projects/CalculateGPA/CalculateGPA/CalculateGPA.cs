9091bd7567c4204a3952d2996f0a9041c4d5e1e4
Ben Davis <contact.warpzone@gmail.com>
2018-07-18 19:14:13 -0600

    Finish Ch 7 assignments

---
+++ Ch7Projects/CalculateGPA/CalculateGPA/CalculateGPA.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateGPA
+{
+    class CalculateGPA
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            int totalGrade = 0;
+            int test;
+
+            Console.WriteLine("Please enter a student's four test scores.\n");
+            for (test = 1; test <= 4; test++)
+            {
+                Console.Write("Please input student's grade for test #{0} ", test);
+                input = Convert.ToInt32(Console.ReadLine());
+                totalGrade += input;
+            }
+            int gPA = totalGrade / (test - 1);
+            switch (gPA/10)
+            {
+                case 9:
+                case 10:
+                    Console.WriteLine("This student's GPA is 4.");
+                    break;
+                case 8:
+                    Console.WriteLine("This student's GPA is 3.");
+                    break;
+                case 7:
+                    Console.WriteLine("This student's GPA is 2.");
+                    break;
+                case 6:
+                    Console.WriteLine("This student's GPA is 1.");
+                    break;
+                default:
+                    Console.WriteLine("This student's GPA is 0.");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ch7Projects/GuessingGame/GuessingGame/GuessingGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class GuessingGame
+    {
+        static void Main(string[] args)
+        {
+            Random rNG = new Random();
+            int number = rNG.Next(1, 101);
+            int guess = 0;
+            int count = 1;
+            string response;
+
+            Console.Write("Guess a whole number between 1 and 100. Press q to quit.\n" +
+                "What number am I thinking of? ");
+            response = Console.ReadLine();
+
+            while (!response.Equals('q') && number != guess)
+            {
+                guess = Convert.ToInt32(response);
+
+                if (guess == number)
+                {
+                    Console.Write("Congrats! You win!\n" +
+                        "Number of Guesses = {0}\n" +
+                        "Press any key to quit.", count);
+                    Console.ReadKey();
+                    break;
+                }   // win the game
+                else if (guess < number)
+                    Console.WriteLine("That guess was too low.");
+                else
+                    Console.WriteLine("That guess was too high.");
+
+                count++;
+                Console.Write("Don't give up now! Guess again! (but if you do want to give up now, press q) ");
+                response = Console.ReadLine();
+            }
+        }
+    }
+}
